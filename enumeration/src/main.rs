73f55ec66d808134e350461297c6bb453569db7e
marsevilspirit <marsevilspirit@gmail.com>
2024-09-24 21:52:52 +0800

    add something

--- enumeration/src/main.rs
+++ enumeration/src/main.rs
@@ -1,3 +1,22 @@
+enum PokerSuit {
+    Clubs,
+    Spades,
+    Diamonds,
+    Hearts,
+}
+
+struct PokerCard {
+    suit: PokerSuit, 
+    value: u8,
+}
+
 fn main() {
-    println!("Hello, world!");
+    let c1 = PokerCard { 
+        suit: PokerSuit::Clubs, 
+        value: 1 
+    };
+    let c2 = PokerCard { 
+        suit: PokerSuit::Hearts, 
+        value: 12
+    };
 }
